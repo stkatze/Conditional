6fb8b2b918d08514c8a15b3a2b6036e689488f31
st.katze <st.katze@icloud.com>
2023-09-08 21:43:13 +0300

    Fixed project

---
+++ src/main/java/ru/netology/ConditionalApplication.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionalApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/netology/profile/DevProfile.java
@@ -0,0 +1,9 @@
+package ru.netology.profile;
+
+public class DevProfile implements SystemProfile {
+
+    @Override
+    public String getProfile() {
+        return "Current profile is dev";
+    }
+}
